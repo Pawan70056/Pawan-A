40542e9006b48c5ff3554741917d0ffd26dc823f
unknown <ssmp70056@gmail.com>
2022-09-07 07:32:37 +0545

    project worked

---
+++ intro.py
@@ -0,0 +1,16 @@
+# this is a single line comment (single line comment)
+""" 
+this is a muti line comment we can write many 
+lines here as much as you want 
+no matter 
+This all above lines are consider as comment in python
+"""
+from email.headerregistry import Address
+
+
+name= input("Enter your name:")
+Address= input("Enter your address:")
+number= input("Enter your contact number:")
+school= input("Enter your school name:")
+gpa= input("Enter your Gpa:")
+print(f" Hi! My name is {name}from {Address} and My Phone number is {number}. I pass my +2 from {school} GPA {gpa}.")
